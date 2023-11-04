898fbb35247d2b36d36d0617955ee592c8f38a23
Cillian <G00404411@atu.ie>
2023-11-04 18:31:20 +0000

    Added notification service open feign

--- src/main/java/ie/atu/registrationservice/RegistrationController.java
+++ src/main/java/ie/atu/registrationservice/RegistrationController.java
@@ -1,5 +1,6 @@
 package ie.atu.registrationservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +8,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RegistrationController {
 
+    private NotificationService notificationService;
+
+    @Autowired
+    public RegistrationController(NotificationService notificationService){
+        this.notificationService = notificationService;
+    }
+
     @PostMapping("/confirm")
     public String confirmDetails(@RequestBody UserDetails userDetails){
         String confirmationMessage = String.format("Received details for %s with email %s",
-                userDetails.getName(), userDetails.getEmail());
+                userDetails.getName(), userDetails.getEmail() + "  " + notificationService.notification(userDetails));
         return confirmationMessage;
     }
+
+
 }
